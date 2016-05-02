03ffd4d2730b2cdf686b127c05cff0b7c771f51b
Zeldroxe <romainfranc@msn.com>
2016-05-02 09:11:03 +0200

    Initial commit

---
+++ manifest.php
@@ -0,0 +1,57 @@
+<?php
+/**  
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * 
+ * Copyright (c) 2016 (original work) Franc Romain;
+ *               
+ * 
+ */               
+
+return array(
+    'name' => 'taoResultsPage',
+	'label' => 'taoResultsPage',
+	'description' => '',
+    'license' => 'GPL-2.0',
+    'version' => '1.0',
+	'author' => 'Franc Romain',
+	'dependencies' => array('tao'),
+	'managementRole' => 'http://www.tao.lu/Ontologies/generis.rdf#taoResultsPageManager',
+    'acl' => array(
+        array('grant', 'http://www.tao.lu/Ontologies/generis.rdf#taoResultsPageManager', array('ext'=>'taoResultsPage')),
+    ),
+    'uninstall' => array(
+    ),
+    'autoload' => array (
+        'psr-4' => array(
+            'oat\\taoResultsPage\\' => dirname(__FILE__).DIRECTORY_SEPARATOR
+        )
+    ),
+    'routes' => array(
+        '/taoResultsPage' => 'oat\\taoResultsPage\\controller'
+    ),    
+	'constants' => array(
+	    # views directory
+	    "DIR_VIEWS" => dirname(__FILE__).DIRECTORY_SEPARATOR."views".DIRECTORY_SEPARATOR,
+	    
+		#BASE URL (usually the domain root)
+		'BASE_URL' => ROOT_URL.'taoResultsPage/',
+	    
+	    #BASE WWW required by JS
+	    'BASE_WWW' => ROOT_URL.'taoResultsPage/views/'
+	),
+    'extra' => array(
+        'structures' => dirname(__FILE__).DIRECTORY_SEPARATOR.'controller'.DIRECTORY_SEPARATOR.'structures.xml',
+    )
+);
